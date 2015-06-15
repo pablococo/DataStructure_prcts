4bdce02d85777657d85c00d82e9ce146fed6166a
Pablo coco <coen11@naver.com>
2015-06-15 21:23:42 +0900

    My First Recursive Function

---
+++ DataStructure_prcts/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  DataStructure_prcts
+//
+//  Created by Pablo coco on 2015. 6. 15..
+//  Copyright (c) 2015년 Pablo coco. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+
+void Recursive(int num){
+    if (num <= 0)
+        return;
+        std::cout<<"Recursive call! "<< num << endl;
+        Recursive(num-1);
+}
+
+int main(int argc, const char * argv[]) {
+    Recursive(3);
+    return 0;
+}
